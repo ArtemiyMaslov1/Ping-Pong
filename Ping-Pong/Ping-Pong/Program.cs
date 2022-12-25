670614cec25c654e80fb32ef42e7fe373d122fa3
Артем Маслов <90208966+ArtemiyMaslov1@users.noreply.github.com>
2022-12-25 17:38:13 +0300

    Add files via upload

---
+++ Ping-Pong/Ping-Pong/Program.cs
@@ -0,0 +1,41 @@
+﻿using static System.Console;
+Ping pi=new Ping();
+Pong po = new Pong();
+PingPong pipo=new PingPong();
+pipo.PingPongEvent += pi.PingPong;
+pipo.PingPongEvent += po.PingPong;
+while (true)
+{
+    pipo.go();
+}
+public delegate void PingPongDelegate();
+class Ping
+{
+    public void PingPong()
+    {
+        WriteLine($"Ping received Pong");
+    }
+}
+class Pong
+{
+    public void PingPong()
+    {
+        WriteLine($"Pong received Ping");
+    }
+}
+
+class PingPong
+{
+    public event PingPongDelegate PingPongEvent;
+    public void go()
+    {
+            if (PingPongEvent != null)
+            {
+                PingPongEvent();
+                Thread.Sleep(1000);
+                
+            }
+
+    }
+
+}
